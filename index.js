d6dd246787c85b93da300610093cfb07fdc338ab
Franklin <ifeanyi.franklin.if@gmail.com>
2022-06-24 04:13:08 +0100

    First commit

---
+++ index.js
@@ -0,0 +1,5 @@
+var name = "My name is Franklin Ifeanyi"
+var height = ", my height is 5.8"
+var country = ", and i am from Nigeria."
+var sentence = name + height + country;
+document.write(sentence)
